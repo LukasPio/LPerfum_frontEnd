b439fe1aceb917727db06f4e9f3313e144036667
= <lucaspio.galvao@gmail.com>
2024-05-06 01:13:51 -0300

    add not found page style

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,5 +1,7 @@
 export default function NotFoundPage() {
-    return (
-        <h1>Essa página não foi encontrada, sinto muito por isso.</h1>
-    )
+  return (
+    <h1 style={{ padding: "0.5rem", fontSize: "2.0rem" }}>
+      404 - Essa página não foi encontrada, sinto muito por isso.
+    </h1>
+  );
 }
