d3610ae0cb1abe9d4de90a887f1d5740c79fee85
= <lucaspio.galvao@gmail.com>
2024-05-06 01:39:49 -0300

    Incress header/navbar responsiveness

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
   return (
-    <h1 style={{ padding: "0.5rem", fontSize: "2.0rem" }}>
-      404 - Essa página não foi encontrada, sinto muito por isso.
-    </h1>
+    <>
+      <h1 style={{ padding: "0.5rem", fontSize: "2.0rem" }}>
+        404 - Essa página não foi encontrada, sinto muito por isso.
+      </h1>
+      <Link to={"/"}>
+        <button
+          style={{
+            marginLeft: "0.5rem",
+            padding: "0.25rem",
+            fontSize: "1.0rem",
+          }}
+        >
+          Retornar a página principal.
+        </button>
+      </Link>
+    </>
   );
 }
